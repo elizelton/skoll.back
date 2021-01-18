0c0bb2bf9f01b6b902c170a326e8ed77527d0da7
erickjr6 <53826278+erickjr6@users.noreply.github.com>
2021-01-18 17:57:55 -0300

    Add files via upload

---
+++ Scripts/3 - triggers update.sql
@@ -0,0 +1,346 @@
+-- Triggers e Functions para Update
+-- Regras Fornecedor
+CREATE FUNCTION fornecedorupdate_trigger() RETURNS trigger AS $fornecedorupdate_trigger$
+    BEGIN
+        IF NEW.tipoFornecedor IS NULL or NEW.tipoFornecedor <= 0 or NEW.tipoFornecedor > 2 THEN
+            RAISE EXCEPTION 'O Tipo de Fornecedor deve ser informado';
+        END IF;
+		IF NEW.fk_IdPessoa IS NULL or NEW.fk_IdPessoa <= 0 THEN
+            RAISE EXCEPTION 'O Fornecedor deve estar atrelado a uma Pessoa';
+        END IF;
+        IF NEW.fk_IdPessoa <> OLD.fk_IdPessoa THEN
+            RAISE EXCEPTION 'A Pessoa atrelada ao fornecedor não pode ser alterada';
+        END IF;
+		IF NEW.Ativo IS NULL THEN
+            NEW.Ativo := true;
+        END IF;
+		RETURN NEW;
+    END;
+  $fornecedorupdate_trigger$ LANGUAGE plpgsql;
+  
+CREATE TRIGGER fornecedorupdate_trigger BEFORE UPDATE ON Fornecedor
+    FOR EACH ROW EXECUTE PROCEDURE fornecedorupdate_trigger();
+	
+-- Regras Cliente
+CREATE FUNCTION clienteupdate_trigger() RETURNS trigger AS $clienteupdate_trigger$
+    BEGIN
+        IF NEW.tipoCliente IS NULL or NEW.tipoCliente <= 0 or NEW.tipoCliente > 2 THEN
+            RAISE EXCEPTION 'O Tipo de Cliente deve ser informado';
+        END IF;
+		IF NEW.fk_IdPessoa IS NULL or NEW.fk_IdPessoa <= 0 THEN
+            RAISE EXCEPTION 'O Cliente deve estar atrelado a uma Pessoa';
+        END IF;
+        IF NEW.fk_IdPessoa <> OLD.fk_IdPessoa THEN
+            RAISE EXCEPTION 'A Pessoa atrelada ao cliente não pode ser alterada';
+        END IF;
+		IF NEW.Ativo IS NULL THEN
+            NEW.Ativo := true;
+        END IF;
+		RETURN NEW;
+    END;
+  $clienteupdate_trigger$ LANGUAGE plpgsql;
+  
+CREATE TRIGGER clienteupdate_trigger BEFORE UPDATE ON Cliente
+    FOR EACH ROW EXECUTE PROCEDURE clienteupdate_trigger();
+	
+-- Regras Telefone
+CREATE FUNCTION telefoneupdate_trigger() RETURNS trigger AS $telefoneupdate_trigger$
+    BEGIN
+        IF NEW.ddd IS NULL or NEW.ddd = '' THEN
+            RAISE EXCEPTION 'O DDD deve ser informado';
+        END IF;
+		IF NEW.telefone IS NULL or NEW.telefone = '' THEN
+            RAISE EXCEPTION 'O Telefone deve ser informado';
+        END IF;
+		IF NEW.fk_IdPessoa IS NULL or NEW.fk_IdPessoa <= 0 THEN
+            RAISE EXCEPTION 'O Telefone deve estar atrelado a uma Pessoa';
+        END IF;
+        IF NEW.fk_IdPessoa <> OLD.fk_IdPessoa THEN
+            RAISE EXCEPTION 'A Pessoa atrelada ao telefone não pode ser alterada';
+        END IF;
+        IF NEW.tipoTelefone IS NULL or NEW.tipoTelefone <= 0 or NEW.tipoTelefone > 3 THEN
+            RAISE EXCEPTION 'O Tipo do Telefone deve ser informado';
+        END IF;
+		RETURN NEW;
+    END;
+  $telefoneupdate_trigger$ LANGUAGE plpgsql;
+  
+CREATE TRIGGER telefoneupdate_trigger BEFORE UPDATE ON Telefone
+    FOR EACH ROW EXECUTE PROCEDURE telefoneupdate_trigger();
+	
+-- Regras Serviço Prestado
+CREATE FUNCTION servicoprestadoupdate_trigger() RETURNS trigger AS $servicoprestadoupdate_trigger$
+    BEGIN
+        IF NEW.nome IS NULL or NEW.nome = '' THEN
+            RAISE EXCEPTION 'O nome do Serviço Prestado deve ser informado';
+        END IF;
+		IF NEW.valorUnitario IS NULL or NEW.valorUnitario <= 0 THEN
+            RAISE EXCEPTION 'O Serviço Prestado deve ter um valor unitário';
+        END IF;
+		IF NEW.fk_IdProduto IS NULL or NEW.fk_IdProduto <= 0 THEN
+            RAISE EXCEPTION 'O Serviço Prestado deve estar atrelado a um Produto';
+        END IF;
+        IF NEW.fk_IdProduto <> OLD.fk_IdProduto THEN
+            RAISE EXCEPTION 'O Produto atrelado ao serviço prestado não pode ser alterado';
+        END IF;
+        IF EXISTS (SELECT 1 FROM ServicoPrestado WHERE nome = NEW.nome AND fk_IdProduto = new.fk_IdProduto) THEN
+            RAISE EXCEPTION 'Nome de serviço já cadastrado para o produto selecionado';
+        END IF;
+        IF NOT EXISTS (SELECT 1 FROM PRODUTO WHERE idProduto = NEW.fk_IdProduto and ativo = true) THEN
+            RAISE EXCEPTION 'Produto não está ativo';
+        END IF;
+		IF NEW.Ativo IS NULL THEN
+            NEW.Ativo := true;
+        END IF;
+		RETURN NEW;
+    END;
+  $servicoprestadoupdate_trigger$ LANGUAGE plpgsql;
+  
+CREATE TRIGGER servicoprestadoupdate_trigger BEFORE UPDATE ON ServicoPrestado
+    FOR EACH ROW EXECUTE PROCEDURE servicoprestadoupdate_trigger();
+	
+-- Regras Contrato Parcela
+CREATE FUNCTION contratoparcelaupdate_trigger() RETURNS trigger AS $contratoparcelaupdate_trigger$
+    BEGIN
+        IF NEW.numParcela IS NULL or NEW.numParcela <= 0 THEN
+            RAISE EXCEPTION 'O número da parcela do contrato deve ser informado';
+        END IF;
+		IF NEW.valorParcela IS NULL or NEW.valorParcela <= 0 THEN
+            RAISE EXCEPTION 'O valor da parcela do contrato deve ser informado';
+        END IF;
+		IF NEW.dataVencimento IS NULL THEN
+            RAISE EXCEPTION 'A data de vencimento da parcela do contrato deve ser informada';
+        END IF;
+		IF NEW.fk_IdContrato IS NULL or NEW.fk_IdContrato <= 0 THEN
+            RAISE EXCEPTION 'A parcela deve estar atrelada a um contrato';
+        END IF;
+        IF NEW.fk_IdContrato <> OLD.fk_IdContrato THEN
+            RAISE EXCEPTION 'O Contrato correspondente a parcela não pode ser alterado';
+        END IF;
+		IF NEW.Situacao IS NULL or NEW.Situacao <= 0 or NEW.Situacao > 3 THEN
+            NEW.Situacao := 1;
+        END IF;
+		RETURN NEW;
+    END;
+  $contratoparcelaupdate_trigger$ LANGUAGE plpgsql;
+  
+CREATE TRIGGER contratoparcelaupdate_trigger BEFORE UPDATE ON ContratoParcela
+    FOR EACH ROW EXECUTE PROCEDURE contratoparcelaupdate_trigger();
+	
+-- Regras Conta Pagar Parcela
+CREATE FUNCTION contapagarparcelaupdate_trigger() RETURNS trigger AS $contapagarparcelaupdate_trigger$
+    BEGIN
+        IF NEW.numParcela IS NULL or NEW.numParcela <= 0 THEN
+            RAISE EXCEPTION 'O número da parcela da conta a pagar deve ser informado';
+        END IF;
+		IF NEW.valorParcela IS NULL or NEW.valorParcela <= 0 THEN
+            RAISE EXCEPTION 'O valor da parcela da conta a pagar deve ser informado';
+        END IF;
+		IF NEW.dataVencimento IS NULL THEN
+            RAISE EXCEPTION 'A data de vencimento da parcela da conta a pagar deve ser informada';
+        END IF;
+		IF NEW.fk_IdContaPagar IS NULL or NEW.fk_IdContaPagar <= 0 THEN
+            RAISE EXCEPTION 'A parcela deve estar atrelada a uma conta a pagar';
+        END IF;
+        IF NEW.fk_IdContaPagar <> OLD.fk_IdContaPagar THEN
+            RAISE EXCEPTION 'A Conta a Pagar correspondente a parcela não pode ser alterado';
+        END IF;
+		IF NEW.Situacao IS NULL or NEW.Situacao <= 0 or NEW.Situacao > 3 THEN
+            NEW.Situacao := 1;
+        END IF;
+		RETURN NEW;
+    END;
+  $contapagarparcelaupdate_trigger$ LANGUAGE plpgsql;
+  
+CREATE TRIGGER contapagarparcelaupdate_trigger BEFORE UPDATE ON ContaPagarParcela
+    FOR EACH ROW EXECUTE PROCEDURE contapagarparcelaupdate_trigger();
+	
+-- Regras Contrato Serviço
+CREATE FUNCTION contratoservicoupdate_trigger() RETURNS trigger AS $contratoservicoupdate_trigger$
+    BEGIN
+        IF NEW.valorUnitario IS NULL or NEW.numParcela <= 0 THEN
+            RAISE EXCEPTION 'O valor unitário do Serviço Prestado no contrato deve ser informado';
+        END IF;
+		IF NEW.quantidade IS NULL or NEW.quantidade <= 0 THEN
+            RAISE EXCEPTION 'A quantidade deve ser informada';
+        END IF;
+		IF NEW.valorTotal IS NULL or NEW.valorTotal < 0 THEN
+            RAISE EXCEPTION 'O valor total não deve ser menor que 0';
+        END IF;
+		IF NEW.fk_IdContrato IS NULL or NEW.fk_IdContrato <= 0 THEN
+            RAISE EXCEPTION 'O registro deve estar atrelado a um contrato';
+        END IF;
+        IF NEW.fk_IdContrato <> OLD.fk_IdContrato THEN
+            RAISE EXCEPTION 'A Contrato correspondente ao serviço prestado não pode ser alterado';
+        END IF;
+		IF NEW.fk_IdServicoPrestado IS NULL or NEW.fk_IdServicoPrestado <= 0 THEN
+            RAISE EXCEPTION 'O registro deve possuir um serviço prestado';
+        END IF;
+        IF NEW.fk_IdServicoPrestado <> OLD.fk_IdServicoPrestado THEN
+            RAISE EXCEPTION 'O serviço prestado não pode ser alterado';
+        END IF;
+        IF EXISTS (SELECT 1 FROM ContratoServico WHERE fk_IdContrato = NEW.fk_IdContrato and fk_IdServicoPrestado = NEW.fk_IdServicoPrestado) THEN
+            RAISE EXCEPTION 'Serviço Prestado já adicionado ao contrato';
+        END IF;
+		RETURN NEW;
+    END;
+  $contratoservicoupdate_trigger$ LANGUAGE plpgsql;
+  
+CREATE TRIGGER contratoservicoupdate_trigger BEFORE UPDATE ON ContratoServico
+    FOR EACH ROW EXECUTE PROCEDURE contratoservicoupdate_trigger();
+	
+-- Regras Contrato Parcela Pagamento
+CREATE FUNCTION contratoparcelapagamentoupdate_trigger() RETURNS trigger AS $contratoparcelapagamentoupdate_trigger$
+    BEGIN
+        IF NEW.dataPagamento IS NULL THEN
+            RAISE EXCEPTION 'A data de pagamento deve ser informada';
+        END IF;
+		IF NEW.valorPagamento IS NULL or NEW.valorPagamento = 0 THEN
+            RAISE EXCEPTION 'O valor de pagamento não deve ser igual a 0';
+        END IF;
+		IF NEW.fk_IdContratoParcela IS NULL or NEW.fk_IdContratoParcela <= 0 THEN
+            RAISE EXCEPTION 'O pagamento deve estar atlelado a uma parcela';
+        END IF;
+        IF NEW.fk_IdContratoParcela <> OLD.fk_IdContratoParcela THEN
+            RAISE EXCEPTION 'A Parcela de contrato correspondente ao pagamento não pode ser alterada';
+        END IF;
+		RETURN NEW;
+    END;
+  $contratoparcelapagamentoupdate_trigger$ LANGUAGE plpgsql;
+  
+CREATE TRIGGER contratoparcelapagamentoupdate_trigger BEFORE UPDATE ON ContratoParcelaPagamento
+    FOR EACH ROW EXECUTE PROCEDURE contratoparcelapagamentoupdate_trigger();
+	
+-- Regras Conta Pagar Parcela Pagamento
+CREATE FUNCTION contapagarparcelapagamentoupdate_trigger() RETURNS trigger AS $contapagarparcelapagamentoupdate_trigger$
+    BEGIN
+        IF NEW.dataPagamento IS NULL THEN
+            RAISE EXCEPTION 'A data de pagamento deve ser informada';
+        END IF;
+		IF NEW.valorPagamento IS NULL or NEW.valorPagamento = 0 THEN
+            RAISE EXCEPTION 'O valor de pagamento não deve ser igual a 0';
+        END IF;
+		IF NEW.fk_IdContaPagarParcela IS NULL or NEW.fk_IdContaPagarParcela <= 0 THEN
+            RAISE EXCEPTION 'O pagamento deve estar atlelado a uma parcela';
+        END IF;
+        IF NEW.fk_IdContaPagarParcela <> OLD.fk_IdContaPagarParcela THEN
+            RAISE EXCEPTION 'A Parcela de conta a pagar correspondente ao pagamento não pode ser alterada';
+        END IF;
+		RETURN NEW;
+    END;
+  $contapagarparcelapagamentoupdate_trigger$ LANGUAGE plpgsql;
+  
+CREATE TRIGGER contapagarparcelapagamentoupdate_trigger BEFORE UPDATE ON ContaPagarParcelaPagamento
+    FOR EACH ROW EXECUTE PROCEDURE contapagarparcelapagamentoupdate_trigger();
+	
+-- Regras Conta Pagar 
+CREATE FUNCTION contapagarupdate_trigger() RETURNS trigger AS $contapagarupdate_trigger$
+    BEGIN
+        IF NEW.mesInicial IS NULL or NEW.mesInicial <= 0 or NEW.mesInicial > 12 THEN
+            RAISE EXCEPTION 'O mês inicial deve ser informado';
+        END IF;
+		IF NEW.numParcelas IS NULL or NEW.numParcelas <= 0 THEN
+            RAISE EXCEPTION 'O número de parcelas deve ser informado';
+        END IF;
+		IF (NEW.valorTotal IS NULL or NEW.valorTotal <= 0) and (NEW.valorMensal IS NULL or NEW.valorMensal <= 0) THEN
+            RAISE EXCEPTION 'O valor total ou mensal deve ser informado';
+        END IF;
+		IF NEW.fk_IdFornecedor IS NULL or NEW.fk_IdFornecedor <= 0 THEN
+            RAISE EXCEPTION 'A conta deve estar atlelada a um fornecedor';
+        END IF;
+        IF NEW.fk_IdFornecedor <> OLD.fk_IdFornecedor THEN
+            RAISE EXCEPTION 'O Fornecedor não pode ser alterado';
+        END IF;
+		IF NEW.fk_IdPessoa IS NULL or NEW.fk_IdPessoa <= 0 THEN
+            RAISE EXCEPTION 'A conta deve estar atlelada a uma Pessoa';
+        END IF;
+        IF NEW.fk_IdPessoa <> OLD.fk_IdPessoa THEN
+            RAISE EXCEPTION 'A Pessoa não pode ser alterada';
+        END IF;
+        IF EXISTS (SELECT 1 FROM CONTAPAGARPARCELAPAGAMENTO T1, CONTRATOPARCELA T2 WHERE T2.fk_IdContaPagar = NEW.idContaPagar and t1.fk_IdContaPagarParcela = t2.idContaPagarParcela and t1.valorPagamento <> 0) THEN
+            IF NEW.valorTotal <> OLD.valorTotal THEN
+                 RAISE EXCEPTION 'Já houve pagamento de parcela(s) para essa Conta. Não é possível alterar o valor total. Para alterar o valor total do contrato é necessário inserir uma nova parcela de ajuste.';
+            END IF;
+        END IF;
+		RETURN NEW;
+    END;
+  $contapagarupdate_trigger$ LANGUAGE plpgsql;
+  
+CREATE TRIGGER contapagarupdate_trigger BEFORE UPDATE ON ContaPagar
+    FOR EACH ROW EXECUTE PROCEDURE contapagarupdate_trigger();
+	
+-- Regras Contrato
+CREATE FUNCTION contratoupdate_trigger() RETURNS trigger AS $contratoupdate_trigger$
+    BEGIN
+		IF NEW.dataInicio IS NULL THEN
+            RAISE EXCEPTION 'A data de início do Contrato deve ser informada';
+        END IF;
+        IF NEW.dataTermino IS NULL THEN
+            RAISE EXCEPTION 'A data de término do Contrato deve ser informada';
+        END IF;
+		IF NEW.periodoMeses IS NULL or NEW.periodoMeses <= 0 THEN
+            RAISE EXCEPTION 'A quantidade de meses do Contrato deve ser informada';
+        END IF;
+		IF NEW.tipoDocumento IS NULL or NEW.tipoDocumento <= 0 or NEW.tipoDocumento > 2 THEN
+            RAISE EXCEPTION 'O tipo de documento deve ser informado';
+        END IF;
+        IF NEW.numParcelas IS NULL or NEW.numParcelas <= 0 THEN
+            RAISE EXCEPTION 'O número de parcelas deve ser informado';
+        END IF;
+		IF NEW.valorTotal IS NULL or NEW.valorTotal <= 0 THEN
+            RAISE EXCEPTION 'O valor total deve ser informado';
+        END IF;
+		IF NEW.fk_IdFormaPag IS NULL or NEW.fk_IdFormaPag <= 0 THEN
+            RAISE EXCEPTION 'A forma de pagamento deve ser informada';
+        END IF;
+		IF NEW.fk_IdVendedor IS NULL or NEW.fk_IdVendedor <= 0 THEN
+            RAISE EXCEPTION 'O Vendedor deve ser informado';
+        END IF;
+        IF NEW.fk_IdVendedor <> OLD.fk_IdVendedor THEN
+            RAISE EXCEPTION 'O Vendedor não pode ser alterado';
+        END IF;
+		IF NEW.fk_IdUsuario IS NULL or NEW.fk_IdUsuario <= 0 THEN
+            RAISE EXCEPTION 'O Usuário que está incluindo deve ser informado';
+        END IF;
+        IF NEW.fk_IdUsuario <> OLD.fk_IdUsuario THEN
+            RAISE EXCEPTION 'O Usuário responsável não pode ser alterado';
+        END IF;
+		IF NEW.fk_IdCliente IS NULL or NEW.fk_IdCliente <= 0 THEN
+            RAISE EXCEPTION 'O Contrato deve estar atlelada a um Cliente';
+        END IF;
+		IF NEW.fk_IdPessoa IS NULL or NEW.fk_IdPessoa <= 0 THEN
+            RAISE EXCEPTION 'O Contrato deve estar atlelada a uma Pessoa';
+        END IF;
+        IF NEW.fk_IdCliente <> OLD.fk_IdCliente THEN
+            RAISE EXCEPTION 'O Cliente não pode ser alterado';
+        END IF;
+        IF NEW.fk_IdPessoa <> OLD.fk_IdPessoa THEN
+            RAISE EXCEPTION 'A Pessoa não pode ser alterada';
+        END IF;
+        IF EXISTS (SELECT 1 FROM FORMAPAGAMENTO WHERE idFormaPag = NEW.fk_IdFormaPag and qtdParcela < NEW.numParcelas) THEN
+            RAISE EXCEPTION 'O número de parcelas do contrato excede o permitido pela forma de pagamento';
+        END IF;
+        IF EXISTS (SELECT 1 FROM CONTRATOPARCELAPAGAMENTO T1, CONTRATOPARCELA T2 WHERE T2.fk_IdContrato = NEW.idContrato and t1.fk_IdContratoParcela = t2.idContratoParcela and t1.valorPagamento <> 0) THEN
+            IF NEW.valorTotal <> OLD.valorTotal THEN
+                 RAISE EXCEPTION 'Já houve pagamento de parcela(s) para esse Contrato. Não é possível alterar o valor total. Para alterar o valor total do contrato é necessário inserir uma nova parcela de ajuste.';
+            END IF;
+        END IF;
+		IF NEW.Ativo IS NULL THEN
+            NEW.Ativo := true;
+        END IF;
+		RETURN NEW;
+    END;
+  $contratoupdate_trigger$ LANGUAGE plpgsql;
+  
+CREATE TRIGGER contratoupdate_trigger BEFORE UPDATE ON Contrato
+    FOR EACH ROW EXECUTE PROCEDURE contratoupdate_trigger();
+
+--Regras Cidades
+CREATE FUNCTION cidadesupdate_trigger() RETURNS trigger as $cidadesupdate_trigger$
+    BEGIN
+        RAISE EXCEPTION 'A Cidade não pode ser alterada';
+    END 
+   $cidadesupdate_trigger$ LANGUAGE plpgsql;
+
+CREATE TRIGGER cidadesupdate_trigger BEFORE UPDATE ON Cidades
+    FOR EACH ROW EXECUTE PROCEDURE cidadesupdate_trigger();   
---
+++ Scripts/4 - triggers delete.sql
@@ -0,0 +1,163 @@
+-- Triggers e Functions para Delete
+--Regras Pessoa
+CREATE FUNCTION pessoadelete_trigger() RETURNS trigger AS $pessoadelete_trigger$
+    BEGIN
+        IF EXISTS (SELECT 1 FROM Fornecedor WHERE fk_IdPessoa = OLD.IdPessoa) OR EXISTS (SELECT 1 FROM Cliente WHERE fk_IdPessoa = OLD.IdPessoa) THEN
+            RAISE EXCEPTION 'Pessoa ja atrelada a um fornecedor ou cliente';
+        END IF;
+        IF EXISTS (SELECT 1 FROM ContaPagar WHERE fk_IdPessoa = OLD.IdPessoa) OR EXISTS (SELECT 1 FROM Contrato WHERE fk_IdPessoa = OLD.IdPessoa) THEN
+            RAISE EXCEPTION 'Pessoa ja atribuída a um Contrato ou Conta a Pagar';
+        END IF;
+		RETURN NEW;
+    END;
+  $pessoadelete_trigger$ LANGUAGE plpgsql;
+  
+CREATE TRIGGER pessoadelete_trigger BEFORE DELETE ON Pessoa
+    FOR EACH ROW EXECUTE PROCEDURE pessoadelete_trigger();
+	
+-- Regras Usuario
+CREATE FUNCTION usuariodelete_trigger() RETURNS trigger AS $usuariodelete_trigger$
+    BEGIN
+        IF EXISTS (SELECT 1 FROM Contrato WHERE fk_IdUsuario = OLD.IdUsuario) THEN
+            RAISE EXCEPTION 'Usuário ja atribuído a um Contrato';
+        END IF;
+		RETURN NEW;
+    END;
+  $usuariodelete_trigger$ LANGUAGE plpgsql;
+  
+CREATE TRIGGER usuariodelete_trigger BEFORE DELETE ON Usuario
+    FOR EACH ROW EXECUTE PROCEDURE usuariodelete_trigger();
+
+-- Regras Fornecedor
+CREATE FUNCTION fornecedordelete_trigger() RETURNS trigger AS $fornecedordelete_trigger$
+    BEGIN
+        IF EXISTS (SELECT 1 FROM ContaPagar WHERE fk_IdFornecedor = OLD.IdFornecedor and fk_IdPessoa = OLD.IdPessoa) THEN
+            RAISE EXCEPTION 'Fornecedor ja atribuído a uma Conta a Pagar';
+        END IF;
+		RETURN NEW;
+    END;
+  $fornecedordelete_trigger$ LANGUAGE plpgsql;
+  
+CREATE TRIGGER fornecedordelete_trigger BEFORE DELETE ON Fornecedor
+    FOR EACH ROW EXECUTE PROCEDURE fornecedordelete_trigger();
+	
+-- Regras Cliente
+CREATE FUNCTION clientedelete_trigger() RETURNS trigger AS $clientedelete_trigger$
+    BEGIN
+        IF EXISTS (SELECT 1 FROM Contrato WHERE fk_IdCliente = OLD.IdCliente and fk_IdPessoa = OLD.IdPessoa) THEN
+            RAISE EXCEPTION 'Cliente ja atribuído a um Contrato';
+        END IF;
+		RETURN NEW;
+    END;
+  $clientedelete_trigger$ LANGUAGE plpgsql;
+  
+CREATE TRIGGER clientedelete_trigger BEFORE DELETE ON Cliente
+    FOR EACH ROW EXECUTE PROCEDURE clientedelete_trigger();
+	
+-- Regras Produto
+CREATE FUNCTION produtodelete_trigger() RETURNS trigger AS $produtodelete_trigger$
+    BEGIN
+        IF EXISTS (SELECT 1 FROM ServicoPrestado WHERE fk_IdProduto = OLD.IdProduto) THEN
+            RAISE EXCEPTION 'Produto ja atribuído a um Serviço Prestado';
+        END IF;     
+		RETURN NEW;
+    END;
+  $produtodelete_trigger$ LANGUAGE plpgsql;
+  
+CREATE TRIGGER produtodelete_trigger BEFORE DELETE ON Produto
+    FOR EACH ROW EXECUTE PROCEDURE produtodelete_trigger();
+	
+-- Regras Serviço Prestado
+CREATE FUNCTION servicoprestadodelete_trigger() RETURNS trigger AS $servicoprestadodelete_trigger$
+    BEGIN
+        IF EXISTS (SELECT 1 FROM ContratoServico WHERE fk_IdServicoPrestado = OLD.idServPrest) THEN
+            RAISE EXCEPTION 'Serviço Prestado ja atribuído a um Contrato';
+        END IF;     
+		RETURN NEW;
+    END;
+  $servicoprestadodelete_trigger$ LANGUAGE plpgsql;
+  
+CREATE TRIGGER servicoprestadodelete_trigger BEFORE DELETE ON ServicoPrestado
+    FOR EACH ROW EXECUTE PROCEDURE servicoprestadodelete_trigger();
+	
+-- Regras Formas de Pagamento
+CREATE FUNCTION formapagamentodelete_trigger() RETURNS trigger AS $formapagamentodelete_trigger$
+    BEGIN
+        IF EXISTS (SELECT 1 FROM Contrato WHERE fk_IdFormaPag = OLD.idFormaPag) THEN
+            RAISE EXCEPTION 'Forma de Pagamento ja atribuída a um Contrato';
+        END IF;  
+		RETURN NEW;
+    END;
+  $formapagamentodelete_trigger$ LANGUAGE plpgsql;
+  
+CREATE TRIGGER formapagamentodelete_trigger BEFORE DELETE ON FormaPagamento
+    FOR EACH ROW EXECUTE PROCEDURE formapagamentodelete_trigger();
+	
+-- Regras Vendedor
+CREATE FUNCTION vendedordelete_trigger() RETURNS trigger AS $vendedordelete_trigger$
+    BEGIN
+        IF EXISTS (SELECT 1 FROM Contrato WHERE fk_IdVendedor = OLD.IdVendedor) THEN
+            RAISE EXCEPTION 'Vendedor ja atribuída a um Contrato';
+        END IF; 
+		RETURN NEW;
+    END;
+  $vendedordelete_trigger$ LANGUAGE plpgsql;
+  
+CREATE TRIGGER vendedordelete_trigger BEFORE DELETE ON Vendedor
+    FOR EACH ROW EXECUTE PROCEDURE vendedordelete_trigger();
+	
+-- Regras Contrato Parcela
+CREATE FUNCTION contratoparceladelete_trigger() RETURNS trigger AS $contratoparceladelete_trigger$
+    BEGIN
+        IF EXISTS (SELECT 1 FROM ContratoParcelaPagamento WHERE fk_IdContratoParcela = OLD.IdContratoParcela) THEN
+            RAISE EXCEPTION 'Parcela já possui um pagamento';
+        END IF; 
+		RETURN NEW;
+    END;
+  $contratoparceladelete_trigger$ LANGUAGE plpgsql;
+  
+CREATE TRIGGER contratoparceladelete_trigger BEFORE DELETE ON ContratoParcela
+    FOR EACH ROW EXECUTE PROCEDURE contratoparceladelete_trigger();
+	
+-- Regras Conta Pagar Parcela
+CREATE FUNCTION contapagarparceladelete_trigger() RETURNS trigger AS $contapagarparceladelete_trigger$
+    BEGIN
+        IF EXISTS (SELECT 1 FROM ContaPagarParcelaPagamento WHERE fk_IdContaPagarParcela = OLD.IdContaPagarParcela) THEN
+            RAISE EXCEPTION 'Parcela já possui um pagamento';
+        END IF; 
+		RETURN NEW;
+    END;
+  $contapagarparceladelete_trigger$ LANGUAGE plpgsql;
+  
+CREATE TRIGGER contapagarparceladelete_trigger BEFORE DELETE ON ContaPagarParcela
+    FOR EACH ROW EXECUTE PROCEDURE contapagarparceladelete_trigger();
+
+-- Regras Conta Pagar 
+CREATE FUNCTION contapagardelete_trigger() RETURNS trigger AS $contapagardelete_trigger$
+    BEGIN
+        RAISE EXCEPTION 'A Conta a Pagar não pode ser excluída';
+    END;
+  $contapagardelete_trigger$ LANGUAGE plpgsql;
+  
+CREATE TRIGGER contapagardelete_trigger BEFORE DELETE ON ContaPagar
+    FOR EACH ROW EXECUTE PROCEDURE contapagardelete_trigger();
+	
+-- Regras Contrato
+CREATE FUNCTION contratodelete_trigger() RETURNS trigger AS $contratodelete_trigger$
+    BEGIN
+		RAISE EXCEPTION 'O Contrato não pode ser excluído';
+    END;
+  $contratodelete_trigger$ LANGUAGE plpgsql;
+  
+CREATE TRIGGER contratodelete_trigger BEFORE DELETE ON Contrato
+    FOR EACH ROW EXECUTE PROCEDURE contratodelete_trigger();
+
+--Regras Cidades
+CREATE FUNCTION cidadesdelete_trigger() RETURNS trigger as $cidadesdelete_trigger$
+    BEGIN
+        RAISE EXCEPTION 'A Cidade não pode ser excluída';
+    END 
+   $cidadesdelete_trigger$ LANGUAGE plpgsql;
+
+CREATE TRIGGER cidadesdelete_trigger BEFORE DELETE ON Cidades
+    FOR EACH ROW EXECUTE PROCEDURE cidadesdelete_trigger();   
