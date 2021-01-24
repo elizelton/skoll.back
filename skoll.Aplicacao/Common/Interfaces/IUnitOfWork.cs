43fe8ec004867668e34c9a5ae340da704b23253b
ecjun <juninhoerick.ej@gmail.com>
2021-01-23 21:47:10 -0300

    IUnitOfWork - Aplicacao completo

--- skoll.Aplicacao/Common/Interfaces/IUnitOfWork.cs
+++ skoll.Aplicacao/Common/Interfaces/IUnitOfWork.cs
@@ -9,6 +9,21 @@ namespace skoll.Aplicacao.Common.Interfaces
     {
         IRepositorio<Usuario> UsuarioRepositorio { get; }
         IRepositorio<Cidade> CidadeRepositorio { get; }
+        IRepositorio<Cliente> ClienteRepositorio { get; }
+        IRepositorio<ContaPagarParcelaPagamento> ContaPagarParcelaPagamentoRepositorio { get; }
+        IRepositorio<ContaPagarParcela> ContaPagarParcelaRepositorio { get; }
+        IRepositorio<ContaPagar> ContaPagarRepositorio { get; }
+        IRepositorio<ContratoParcelaPagamento> ContratoParcelaPagamentoRepositorio { get; }
+        IRepositorio<ContratoParcela> ContratoParcelaRepositorio { get; }
+        IRepositorio<Contrato> ContratoRepositorio { get; }
+        IRepositorio<ContratoServico> ContratoServicoRepositorio { get; }
+        IRepositorio<FormaPagamento> FormaPagamentoRepositorio { get; }
+        IRepositorio<Fornecedor> FornecedorRepositorio { get; }
+        IRepositorio<Pessoa> PessoaRepositorio { get; }
+        IRepositorio<Produto> ProdutoRepositorio { get; }
+        IRepositorio<ServicoPrestado> ServicoPrestadoRepositorio { get; }
+        IRepositorio<Telefone> TelefoneRepositorio { get; }
+        IRepositorio<Vendedor> VendedorRepositorio { get; }
 
         void Commit();
         void RollBack();
